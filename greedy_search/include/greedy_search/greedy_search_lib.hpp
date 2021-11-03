20ec6d4831bd20ad086fc246b8a73487d63c2e2d
sziselman <sarahziselman2020@u.northwestern.edu>
2021-11-03 00:10:36 -0500

    update greedy search library

--- greedy_search/include/greedy_search/greedy_search_lib.hpp
+++ greedy_search/include/greedy_search/greedy_search_lib.hpp
@@ -4,59 +4,36 @@
 #include <geometry_msgs/Pose.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "scene_setup/Block.h"
 
 #include <vector>
 #include <string>
 
 namespace greedy_search
 {
-    /// \brief a 3D object with a position and orientation
-    struct Block
-    {
-        std::vector<double> dimensions;
-        geometry_msgs::Pose pose;
-        int id;
-        double visibility;
-        double execution_time;
-        double utility;
-
-        /// \brief constructor that creates a block located at (0.0, 0.0)
-        /// \return block at origin
-        Block();
-
-        /// \brief constructor that takes a position and orientation
-        /// \param block_pose a six dimensional vector containing position and orientation
-        /// \return a block at specified position and orientation
-        Block(std::vector<double> block_dimensions, geometry_msgs::Pose block_pose, int block_id);
-
-        void updateVisibility(void);
-
-        void updateExecutionTime(void);
-        /// \brief calculates the utility (V/T)
-        /// \return the utility of the block
-        double calculate_utility(void);
-
-    };
-
     /// \brief class for Greedy Search
     /// the greedy algorithm ranks the accessible objects in a scene based on utility and removes the highest utility object
     class GreedySearch
     {
         private:
-            std::vector<Block> objects;     // list of poses (position and orientation) for the visible objects within a scene
-            double num;                     // number of objects visible
+            std::vector<scene_setup::Block> objects;    // list of poses (position and orientation) for the visible objects within a scene
+            double num;                                 // number of objects visible
+        
         public:
             /// \brief constructor for GreedySearch object
+            GreedySearch();
+
+            /// \brief constructor for GreedySearch object
             /// \param blocks : a vector of poses that represent the blocks seen from the RealSense camera
-            GreedySearch(std::vector<Block> & blocks);
+            GreedySearch(std::vector<scene_setup::Block> blocks);
 
             /// \brief updates the objects within the scene
             /// \param block_poses : a vector of poses that represent the blocks seen from the RealSense camera
-            void update_objects(std::vector<Block> & blocks);
+            void updateObjects(std::vector<scene_setup::Block> blocks);
 
             /// \brief gets the optimal arrangement using greedy search
             /// \return a list of blocks in order of arrangement
-            std::vector<Block> get_arrangement(void);
+            std::vector<scene_setup::Block> getArrangement(void);
     };
 }
 
--- greedy_search/src/greedy_search_lib.cpp
+++ greedy_search/src/greedy_search_lib.cpp
@@ -2,67 +2,24 @@
 
 namespace greedy_search
 {
-    Block::Block()
-    {
-        pose.position.x = 0.0;
-        pose.position.y = 0.0;
-        pose.position.z = dimensions[2]/2;
-        
-        tf2::Quaternion quat;
-        quat.setRPY(0.0, 0.0, 0.0);
-        pose.orientation = tf2::toMsg(quat);
+    GreedySearch::GreedySearch() {
+        num = 0;
     }
 
-    Block::Block(std::vector<double> block_dimensions, geometry_msgs::Pose block_pose, int block_id)
-    {
-        dimensions = block_dimensions;
-        pose = block_pose;
-        id = block_id;
-    }
-
-    double Block::calculate_utility(void)
-    {
-        // the number of target poses that become visible once the object is removed
-        double visibility = 0;
-
-        // estimate of time necessary to execute the trajectory of the robot
-        double execution_time = 1;
-
-        // the estimated utility of the block
-        return visibility / execution_time;
-    }
-
-    GreedySearch::GreedySearch(std::vector<Block> & blocks)
-    {
+    GreedySearch::GreedySearch(std::vector<scene_setup::Block> blocks) {
         num = blocks.size();
         objects = blocks;
     }
 
-    void GreedySearch::update_objects(std::vector<Block> & blocks)
-    {
+    void GreedySearch::updateObjects(std::vector<scene_setup::Block> blocks) {
         num = blocks.size();
         objects = blocks;
     }
 
-    std::vector<Block> GreedySearch::get_arrangement(void)
-    {
-        for (int i = 0; i < objects.size()-1; i++) {
-            for (int j = i+1; j < objects.size(); j++) {
-
-                // calculate the utility
-                double utility1 = objects[i].calculate_utility();
-                double utility2 = objects[j].calculate_utility();
-
-                // if the utility of ind i is greater than utility of ind j, swap
-                if (utility1 > utility2) {
-                    Block temp = objects[i];
-                    objects[i] = objects[j];
-                    objects[j] = temp;
-                }
-            }
-        }
+    std::vector<scene_setup::Block> GreedySearch::getArrangement(void) {
+        std::vector<scene_setup::Block> arrangement;
         
-        return objects;
+        return arrangement;
     }
 
 }
